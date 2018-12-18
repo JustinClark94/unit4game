6dbb9fa63a1f88ab34fb4c05dc33e724f1214104
Justin Clark <justclark.jose@gmail.com>
2018-12-18 17:30:26 -0600

    commiting changes

--- javascript/game.js
+++ javascript/game.js
@@ -1,69 +1,76 @@
 
 
-//random number that player needs to match
-var targetNumber;
-//scoreboard
-var wins = 0;
-var losses = 0;
-//values of the 4 crystals added together
-var crystalTotal = 0;
-//four crystals
-var ac ;
-var bc ;
-var cc ;
-var dc ;
+  //random number that player needs to match
+  var targetNumber;
+  //scoreboard
+  var wins = 0;
+  var losses = 0;
+  //values of the 4 crystals added together
+  var crystalTotal = 0;
+  //four crystals
+  var ac ;
+  var bc ;
+  var cc ;
+  var dc ;
 
-//Assigns a random number to achieve. and a a random number to each crystal.
 
-targetNumber = [Math.floor(Math.random() * 150) + 27];
-ac = [Math.floor(Math.random() * 12) + 1];
-bc = [Math.floor(Math.random() * 12) + 1];
-cc = [Math.floor(Math.random() * 12) + 1];
-dc = [Math.floor(Math.random() * 12) + 1];
+  //Assigns a random number to achieve. and a a random number to each crystal.
 
-$("totalScore").html(crystalTotal);
-   //crystalTotal = parseInt(crystalTotal)
-$("#randomNumber").html("Target number: " + targetNumber);
-$("#winning").html(wins);
-$("#losing").html(losses);
+  targetNumber = (Math.floor(Math.random() * 150) + 27);
+  ac = (Math.floor(Math.random() * 12) + 1);
+  bc = (Math.floor(Math.random() * 12) + 1);
+  cc = (Math.floor(Math.random() * 12) + 1);
+  dc = (Math.floor(Math.random() * 12) + 1);
 
-//console log to make sure all number generators are working.
-console.log(targetNumber);
-console.log(ac,bc,cc,dc);
+  $("totalScore").html(crystalTotal);
+    //crystalTotal = parseInt(crystalTotal)
+  $("#randomNumber").html("Target number: " + targetNumber);
+  $("#winning").html(wins);
+  $("#losing").html(losses);
 
-var scoreCheck = function(){
-   if (crystalTotal===targetNumber){
+  //console log to make sure all number generators are working.
+  console.log(targetNumber);
+  console.log(ac,bc,cc,dc);
+
+  //THIS SECTION IS INCOMPLETE!!!
+    var checkScore = function(){
+    if (crystalTotal===targetNumber){
+      wins ++  
       //alert("Congrats! You won the game!");
-      wins++
-   }
-   else if (crystalTotal > targetNumber){
+      console.log("W" + wins)
+        
+    }
+    else if (crystalTotal > targetNumber){
+      losses ++  
       //alert("You lost! Try Again");
-      loses++
-   }
+      console.log("L" + losses)
+        
+    }
+  };
 
-}
 
+    
 
-  
-// += should sum the values but its adding it like a string...
- $( "#acrystal" ).click(function() {
-   crystalTotal += ac;
-   $('#totalScore').html(crystalTotal)
-   scoreCheck();
- });
- $( "#bcrystal" ).click(function() {
-   crystalTotal += bc;
-   $('#totalScore').html(crystalTotal)
-   scoreCheck();
- });
- $( "#ccrystal" ).click(function() {
-   crystalTotal += cc;
-   $('#totalScore').html(crystalTotal)
-   scoreCheck();
- });
- $( "#dcrystal" ).click(function() {
-   crystalTotal += dc;
-   $('#totalScore').html(crystalTotal)
-   scoreCheck();
- }); 
-console.log(scoreCheck)
+  $( "#acrystal" ).click(function() {
+    crystalTotal += ac;
+    $('#totalScore').html(crystalTotal)
+    checkScore();
+  });
+  $( "#bcrystal" ).click(function() {
+    crystalTotal += bc;
+    $('#totalScore').html(crystalTotal)
+    checkScore();
+  });
+  $( "#ccrystal" ).click(function() {
+    crystalTotal += cc;
+    $('#totalScore').html(crystalTotal)
+    checkScore();
+  });
+  $( "#dcrystal" ).click(function() {
+    crystalTotal += dc;
+    $('#totalScore').html(crystalTotal)
+    checkScore();
+  }); 
+
+  //Score appears in console but not on page.
+  //Need to reset the game without losing score
